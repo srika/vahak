0bea49dbf33e73c08852fa40f91c91b9f0fa00eb
srika <srikanthnayak583@gmail.com>
2021-05-08 16:16:49 -0400

    form design

---
+++ src/components/Card/PersonalDetails.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PersonalDetails = ({ showDetails }) => {
+  return (
+    <div
+      className={`card-container__person-details ${!showDetails && "d-none"}`}
+    >
+      <div className="card-container__person-details__avatar">
+        <i className="far fa-user-circle fa-6x"></i>
+      </div>
+      <div className="card-container__person-details__info">
+        <h2 className="card-container__person-details__info__name">
+          Rohan Sharma
+        </h2>
+        <p className="card-container__person-details__info__location">
+          Bengaluru, Karnataka
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalDetails;
---
+++ src/components/Form/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../Header";
+
+const Form = () => {
+  return (
+    <div className="form-container">
+      <Header title="Place Your Load BID" className="fs-18"/>
+    </div>
+  );
+};
+
+export default Form;
